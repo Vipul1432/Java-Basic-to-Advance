5f14b4bd64d174db7a2af402f39203584e838759
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-02-28 08:47:11 +0530

    BitwiseAnd

---
+++ BitwiseAnd.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class BitwiseAnd {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter a: ");
+      int a = sc.nextInt();
+      System.out.print("Enter b: ");
+      int b = sc.nextInt();
+      int c = a & b;
+        System.out.println("Bitwise of a & b is: " +c);
+    }
+}
