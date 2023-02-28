80eb2fbfb78168fe692fb9f50e9df9a01a67f936
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-02-28 08:57:08 +0530

    BreakDemo

---
+++ BreakDemo.java
@@ -0,0 +1,13 @@
+public class BreakDemo {
+       public static void main(String[] args ) {
+      
+       int i;
+       for (i = 0; i < 10; i++) {
+          
+       if (i == 4) {
+       break;
+       }
+             System.out.println(i);
+             }
+       }
+}
