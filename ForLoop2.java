b7e1167cdfd8c9b2d49cd0f05c24fc716d7330ff
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-03-02 22:47:38 +0530

    ForLoop2

---
+++ ForLoop2.java
@@ -0,0 +1,12 @@
+public class ForLoop2 {
+       public static void main(String [] args) {
+       
+       for (int i = 1; i <100; i++) {
+       int j = 1;
+       if(j % 2 == 0)
+              {
+                System.out.println(i);
+              }
+          }
+     }
+}
