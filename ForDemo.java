5d526c9bfa52f3944a6ff3657909e36c59f5b300
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-03-02 22:45:16 +0530

    ForDemo

---
+++ ForDemo.java
@@ -0,0 +1,8 @@
+public class ForDemo {
+       public static void main(String [] args) {
+       
+       for (int i = 10; i <5; i -=2) {
+           System.out.println(i);
+           }
+       }
+}
