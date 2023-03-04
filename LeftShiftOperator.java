757541864d413ac6a66dfa197c9edb2c2c871c20
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-03-04 09:21:15 +0530

    LeftShiftOperator

---
+++ LeftShiftOperator.java
@@ -0,0 +1,9 @@
+class LeftShiftOperator{
+      public static void main(String[] agrs){
+      
+      System.out.println(10<<2);
+      System.out.println(10<<3);
+      System.out.println(20<<2);
+      System.out.println(15<<4);
+      }
+}
