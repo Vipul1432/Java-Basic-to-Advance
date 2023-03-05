8fc0dcb4b44204458bb55ee6a54b2550578b0680
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-03-05 10:20:06 +0530

    ManualTypeCasting

---
+++ ManualTypeCasting.java
@@ -0,0 +1,7 @@
+public class ManualTypeCasting {
+    public static void main(String[] args) {
+        double myDouble = 3.14;
+        int myInt = (int)myDouble;
+        System.out.print(myInt);
+    }
+}
