fb2315ded3107e2b8869b69642f720d25495973b
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-03-05 10:22:01 +0530

    MathDemo2

---
+++ MathDemo3.java
@@ -0,0 +1,37 @@
+public class MathDemo3 {
+    public static void main(String[] args) {
+
+        double a = 30;
+
+        // converting values to radian
+
+        double b = Math.toRadians(a);
+
+        // return the trigonometric sine of a
+        System.out.println("Sine value of a is : " + Math.sin(a));
+
+        // return the trigonometric cosine of a
+        System.out.println("Cosine value of a is : " + Math.cos(a));
+
+        // return the trigonometric Tangent of a
+        System.out.println("Tangent value of a is : " + Math.tan(a));
+
+        // return the trigonometric arc sine of a
+        System.out.println("Sine value of a is : " + Math.asin(a));
+
+        // return the trigonometric arc cosine of a
+        System.out.println("Cosine value of a is : " + Math.acos(a));
+
+        // return the trigonometric arc Tangent of a
+        System.out.println("Tangent value of a is : " + Math.atan(a));
+
+        // return the trigonometric hyperbolic sine of a
+        System.out.println("Sine value of a is : " + Math.sinh(a));
+
+        // return the trigonometric hyperbolic cosine of a
+        System.out.println("Cosine value of a is : " + Math.cosh(a));
+
+        // return the trigonometric hyperbolic Tangent of a
+        System.out.println("Tangent value of a is : " + Math.tanh(a));
+    }
+} 
