7accc567898cfe6ef2171488fea5c38f12f8fe01
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-03-05 10:21:34 +0530

    MathDemo2

---
+++ MathDemo2.java
@@ -0,0 +1,23 @@
+public class MathDemo2 {
+       public static void main(String [] args) {
+      
+       // return the power of 2 raise to 3
+       System.out.println("Power of 2 raise to 3: " + Math.pow(2,3));
+
+       // input positive value , Output ceil value of x
+       System.out.println("Ceil value of -37.34 = " + Math,ceil (-37.34));
+    
+       // print the absolute value of int type 
+       System.out.println("Abs value of -37 = " + Math.abs (-37));
+       System.out.println("Abs value of 37 = " + Math.abs (37));
+
+       // find the closest int for the double 
+       System.out.println("Round value of 37.34 = " + Mathround (-37.34));
+
+       // Input positive value, Output floor value of x
+       System.out.println("Floor value of -37.34 = " + Math.floor(-37.34));
+
+       // Input negative zero, Output round the x 
+       System.out.println("Rint value of -37.34 = "+ Math.rint (-37.34));
+       }
+}
