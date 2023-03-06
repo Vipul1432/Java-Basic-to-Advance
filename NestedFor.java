708cae0a849effbbffeb7160b4aa4c2818338c72
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-03-06 08:23:38 +0530

    NestedFor

---
+++ NestedFor.java
@@ -0,0 +1,13 @@
+public class NestedFor {
+       public static void main(String[] args) {
+       
+       int row, col;
+       for (row = 1; row <= 5; row++) {
+            for (col = 1; col <= 5; col++)
+            {
+            System.out.println(row + " ");
+            }
+        System.out.println("\n");
+        }
+    }
+}
